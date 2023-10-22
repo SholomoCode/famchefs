33f8bd0688a5d463b8b31e3d9c9be35281f8205b
SholomoCode <worksolom5@gmail.com>
2023-10-21 20:26:27 -0500

    Add project files.

---
+++ famchefs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace famchefs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
